b883270b23347b561cf664fd10b41ec833ac436f
david waza <davidyega69@gmail.com>
2024-11-24 18:34:43 +0100

    initial commit

---
+++ .next/static/webpack/pages/our-people2.eab85fee35dbbdc4.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/our-people2",{
+
+/***/ "./src/data/header.json":
+/*!******************************!*\
+  !*** ./src/data/header.json ***!
+  \******************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('[{"id":"1","label":"Home","link":"/index4","icon":false},{"id":"2","label":"Service","link":"/service2","icon":false},{"id":"3","label":"About","link":"#","icon":true,"subMenu":[{"id":"1","label":"About","link":"/about"},{"id":"4","label":"Our People","link":"/our-people2","icon":false},{"id":"6","label":"Event","link":"/event","icon":true,"subMenu":[{"id":"1","label":"Upcoming Event","link":"/event"},{"id":"2","label":"Event Details","link":"/event-details"}]},{"id":"10","label":"FAQs","link":"/faq"},{"id":"11","label":"Terms & Conditions","link":"/terms-conditions"}]},{"id":"5","label":"Contact Us","link":"/contact"}]');
+
+/***/ })
+
+});
